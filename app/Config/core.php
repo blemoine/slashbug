6dade68d9587cce1f2c62e03ed18bbc557015ca0
Benoit Lemoine <blemoine@xebia.fr>
2012-11-14 21:15:51 +0100

    Init Phing

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'hierjaimangeunepommequietaitunegrannysmith');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '170919840311198244442345678976543');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -222,7 +222,7 @@
  * The classname and database used in CakePHP's
  * access control lists.
  */
-	Configure::write('Acl.classname', 'DbAcl');
+//	Configure::write('Acl.classname', 'DbAcl');
 	Configure::write('Acl.database', 'default');
 
 /**
